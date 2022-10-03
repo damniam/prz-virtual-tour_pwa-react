e348eb4159b991190cce5c4a02c11e81f6a4aea7
Damian <ddzialo28@interia.pl>
2022-10-03 18:29:05 +0200

    Update Information

--- src/components/Information.js
+++ src/components/Information.js
@@ -12,40 +12,16 @@ function Information() {
       <h2 className="noselect">Informacje</h2>
       <p>
         Wirtualny spacer po budynku A Wydzialu Elektrotechniki i Informatyki
-        Politechniki Rzeszowskiej.{" "}
-        <strong>Dostępnych jest ponad 100 panoram.</strong>
+        Politechniki Rzeszowskiej.
+        Dostępnych jest <strong> ponad 100 panoram</strong>
         Za pośrednictwem wirtualnego spaceru mozesz zwiedzić nie tylko budynek,
         ale także wnętrza sal wykładowych, laboratoria oraz pracownie
         komputerową. Każda perspektywa pozwala użytkownikowi uzyskanie pełnego
-        widoku obiektu (obrót o 360o). By uzyskać pożądany efekt wystarczy
+        widoku obiektu (obrót o 360). By uzyskać pożądany efekt wystarczy
         przytrzymać lewy przycisk myszy i poruszać kursorem w wybranym kierunku.
         Aplikacja wirtualnego spaceru wraz z multimedialną grą oraz stroną
         internetową stworzona w ramach pracy magisterskiej przez Damiana Działo.
         Zdjęcia zostały wykonane przez Damiana Działo.
-      </p>
-      <p>
-        Cupidatat est consectetur occaecat nostrud in veniam. Deserunt sint non
-        tempor qui non labore esse deserunt qui cillum quis consectetur. Veniam
-        adipisicing officia sint Lorem Lorem velit. Ea proident elit qui nostrud
-        ad incididunt. Labore Lorem aliqua et est consequat. In elit duis eu ex
-        labore consequat dolor. Qui ullamco ea sit velit consectetur aute
-        cupidatat fugiat ad.Mollit reprehenderit do excepteur nisi laborum nisi
-        sit aliqua consequat. Magna ullamco elit proident id occaecat do et amet
-        ea labore non est amet et. Nisi nostrud tempor excepteur aute est in
-        magna minim sit nulla aute minim ad labore. Lorem adipisicing sunt
-        aliqua cillum dolor veniam in consectetur dolore dolor qui ullamco. Nisi
-        qui ad amet in dolore reprehenderit sunt veniam dolor pariatur. Tempor
-        nisi veniam est esse id incididunt consectetur irure tempor aliqua
-        pariatur. Amet labore ut anim pariatur deserunt eiusmod nulla do duis
-        commodo cupidatat sint.
-      </p>
-      <p>
-        Incididunt consectetur eu qui dolor irure nisi laborum culpa dolor
-        ipsum. Proident nulla in sit aliqua ad sit nisi nostrud. Laboris commodo
-        id ipsum do eu aute reprehenderit fugiat ex quis. Anim esse occaecat ad
-        eu occaecat exercitation proident ullamco elit irure. Proident elit ex
-        eiusmod irure quis. Lorem consequat proident fugiat et sint elit minim
-        fugiat est ex commodo aliqua. Irure est esse aliquip mollit.
       </p>
     </Wrapper>
   );
